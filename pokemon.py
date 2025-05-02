094d3bc1c6c1572839b26076a59205d53dc2a6f2
lorenzo-confalonieri <confalonieri.lorenzo@iisgalvanimi.edu.it>
2025-05-02 08:23:28 +0000

    commit 2

--- pokemon.py
+++ pokemon.py
@@ -68,4 +68,4 @@ def mostra_punti_route():
     return render_template('punti.html', punti_utente=punti_utente)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)     
